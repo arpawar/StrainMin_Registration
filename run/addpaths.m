4ff813e8d9e87c0bc3ca513a20a376b689b4f063
Aishwarya Pawar <aishwarya13992@gmail.com>
2021-11-16 15:33:34 -0500

    Add files via upload

---
+++ run/addpaths.m
@@ -0,0 +1,11 @@
+function addpaths()
+% This function adds the folders of the package to the path for running
+% succesfully
+addpath '../store_phi_functions'
+addpath '../run/post_processing'
+addpath '../bspline-util'
+addpath '../thb_refinement'
+addpath '../setparameter_files'
+addpath '../iteration_loop_func'
+addpath '../images'
+end

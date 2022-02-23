deb4af6196b519b8920a103298a2c02b0d59a9ec
Aishwarya Pawar <aishwarya13992@gmail.com>
2022-02-23 16:14:04 -0500

    Delete iteration_loop_func directory

--- iteration_loop_func/compute_phi_cp.m
+++
@@ -1,8 +0,0 @@
-%% Nearest neighbour interpolation
-function phi_cp = compute_phi_cp(Pm,Img)
-for i =1:size(Pm(1,1).pts,1)
-    
-    phi_cp(i,1) = Img(round(Pm(1,1).pts(i,1)), round(Pm(1,1).pts(i,2)), round(Pm(1,1).pts(i,3)));
-    
-end
-end
